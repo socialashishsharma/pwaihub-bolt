ee4dd2193370c6bdbb1bd80fe86f3638786d2ff9
socialashishsharma <socialashishsharma@gmail.com>
2024-12-30 21:12:04 +0530

    opyimizeDeps duplication solved

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,13 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react']
+    exclude: ['lucide-react'],
+    include: ['pdfjs-dist/build/pdf.worker.min.js']
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  optimizeDeps: {
-    include: ['pdfjs-dist/build/pdf.worker.min.js']
   },
   build: {
     rollupOptions: {
